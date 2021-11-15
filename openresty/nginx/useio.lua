b496bd1f2aefd8efa08712ca26fc65a3f4a5a323
root <test@test.com>
2021-11-15 14:28:11 +0800

    openresty

---
+++ openresty/nginx/useio.lua
@@ -0,0 +1,10 @@
+file = io.open("list.json","r")
+io.input(file)
+print(io.read("*a"))
+print(type(io.read("*a")))
+local x = io.read("*a")
+tbl = cjson.encode(x)
+print(type(tbl))
+--tbl = cjson.encode(x)
+--print(type(tbl))
+
